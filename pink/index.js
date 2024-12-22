567f0b9231c1a733a1690b5af17981bee03e28e4
p6nj <breee@duck.com>
2024-12-22 01:52:09 -0500

    pink: incomplete fight

--- pink/index.js
+++ pink/index.js
@@ -4,21 +4,58 @@ var title_variant = 0;
 const hex2name = (hex) => ntc.name(hex)[1];
 
 // write color name to figure caption
-const write_colorname = () => {
+const write_colorname = (name) => {
   document.getElementById("caption").innerHTML =
-    "I'm <strong>" +
-    hex2name(document.getElementById("color-input").value).toLowerCase() +
-    "</strong>.";
+    "I'm <strong>" + name + "</strong>.";
 };
 
-const hide_color_picker = () => {
-  document.getElementById("color-picker").hidden = true;
+const color_picker_visible = (bool) => {
+  document.getElementById("color-picker").hidden = !bool;
+};
+
+const change_image = (link) => {
+  document.getElementById("pic").src = link;
+};
+
+const apply_gradient = (hex) => {
+  const pic = document.getElementById("pic").style;
+  pic.background = `
+radial-gradient(
+  closest-corner at 20% 50%,
+  ${hex},
+  ${hex},
+  ${hex},
+  transparent,
+  transparent,
+  transparent,
+  transparent
+) no-repeat`;
+  pic.backgroundPosition = "left";
+  pic.backgroundSize = "80% 130%";
 };
 
 // color was entered, process it
 const results = () => {
-  hide_color_picker();
-  write_colorname();
+  color_picker_visible(false);
+  const hex_color = document.getElementById("color-input").value;
+  const color_name = hex2name(hex_color).toLowerCase();
+  apply_gradient(hex_color);
+  write_colorname(color_name);
+  change_image("https://fish.golf/pink/img/full-draw.png");
+};
+
+const reset_background = () => {
+  const pic = document.getElementById("pic").style;
+  pic.background = "none";
+};
+
+const reset_caption = () => {
+  document.getElementById("caption").innerHTML = "";
+};
+
+const reset = () => {
+  reset_background();
+  color_picker_visible(true);
 };
 
 const rotate_title = () => {
