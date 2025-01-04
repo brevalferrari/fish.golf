7a5485d084d1273507efd50fa4b7031d6326e7bd
p6nj <breee@duck.com>
2025-01-04 13:02:57 -0500

    p6nj: French translation

---
+++ p6nj/fr.js
@@ -0,0 +1,77 @@
+const translation = {
+  "menu-info": "Info",
+  "menu-location": "Localisation",
+  "menu-status": "News",
+  "menu-books": "Livres",
+  "menu-series": "Séries",
+  "menu-music": "Musique",
+  "menu-links": "Liens",
+  "menu-pics": "Photos",
+  "menu-drawings": "Dessins (secret)",
+  persona:
+    "C'est le nom de ma présence sur internet. Il vient d'une <a href='https://youtu.be/Cu6wlaaf-ck'>vidéo</a> étrange sur la balle du jeu vidéo <a href='https://www.ponggame.org/'>Pong</a>.",
+  description:
+    "Je suis un long Français pan et non-binaire (pronoms masculins) qui fait de son mieux pour aider tout le monde dans sa quête principale : trouver une place dans ce monde énorme.",
+  pronouns:
+    "Vous pouvez consulter ma page Pronouns (en anglais) <a href='https://en.pronouns.page/@p6nj' title='Pronouns'>ici</a> si vous voulez.",
+  home: "Voici un lien vers la page d'accueil :",
+  apioform:
+    "Je me sens obligé de vous proposer également ce lien (page en anglais) :",
+  location: "Localisation",
+  "location-p": "Voici mon emplacement actuel :",
+  status: "News",
+  "news-helloworld-h": "29/12/2024 - Bonjour, Monde",
+  "news-helloworld-p":
+    "Mes premières news. Je suis actuellement étudiant en informatique à l'Université du Québec à Chicoutimi en programme d'échange. Je cherche activement un stage au Québec et dans les provinces voisines. C'est encore les vacances donc j'ai du temps pour nourrir mon site.",
+  "facts-p": "Voici quelques infos sur moi :",
+  "facts-carl": `<a href="https://www.youtube.com/@CarlSagan42">CarlSagan42</a> m'a appris l'anglais (fan depuis ~2016 <3)`,
+  "facts-you": "T'es génial",
+  "facts-ptype": `Je suis <a href="https://www.16personalities.com/profiles/enfp-t/o/3xd6htki3">ENFP-T</a>!`,
+  "facts-nolie": "Je ne mens jamais",
+  "facts-lie": "Je mens parfois",
+  "facts-real": "J'existe (peut-être)",
+  "facts-orchestra":
+    "Il arrive que certains des musiciens d'un grand orchestre se sentent inutiles. Ils font bien partie de l'orchestre mais ils ont l'impression de ne pas être entendu. Pourtant, on sent bien une différence quand ils ne sont pas là. Parfois, si tu te sens comme un violoniste perdu dans un grand orchestre, souviens-toi que tu fais une différence. La vie pour beaucoup d'entre nous ne serait pas la même sans toi. La prochaine fois que tu vois un orchestre jouer, écoute bien et tu les entendra tous. Ils ne font qu'un.",
+  "facts-gentoo":
+    "J'utilise Gentoo après avoir essayé environ 18 distributions (j'ai du temps libre)",
+  "facts-kernel":
+    "Non, je n'ai pas mon propre kernel ! Juste quelques patchs...",
+  books: "Livres",
+  "books-p": `Quand j'étais un petit chimpanzé je lisais des nouvelles de Ray Bradbury traduites en français. Aujourd'hui, mon livre préféré (ou plutôt ma série de livres) c'est le Guide de l'Auto-stoppeur Inter-Galactique ("Hitch-Hiker's Guide to the Galaxy") de Douglas Adams (en anglais).<br />
+  Je lis aussi un peu de Cory Doctorow.`,
+  series: "Séries",
+  "series-p":
+    "Je suis un grand fan de Legion FX. Côté anime, j'adore Lain. Nichijou c'est le truc le plus cute que j'aie jamais vu. Azumanga Daioh est génial. Evangelion est incroyable.",
+  music: "Musique",
+  "music-tastes": `Les silly playlists. C'est mon gaz. Garçon, un <a href="https://youtu.be/q4i05EaML44">glorp</a> s'il-vous-plait ! Plus de <a href="https://youtu.be/cblmv35uMDM">Glumbo</a> aussi !!<br />
+  Le vaporwave, mon ami de longue date. J'adore les effets et mon artiste préféré là-dessus c'est <a href="https://www.youtube.com/watch?v=YTRHBA7Ld0k">Telepath</a>.`,
+  "music-play": `J'aime aussi... Jouer de la musique !<br />
+  J'ai un niveau "professionnel" (payé trois fois 💪), je peux chanter à peu près de tout (entraîné sur du Queen) et je joue aussi en orchestre depuis un bon moment mais plutôt des claviers durs : xylo, vibraphone, cloches tubulaires, glockenspiel et marimba ! Et du triangle !<br />
+  Tu peux me voir fouetter la scène avec une chaîne en métal ici sur <a href="https://youtu.be/CnY2KjfxM5k">YouTube</a>.`,
+  "music-make": `J'ai fait un peu de musique sur Bandcamp mais le dernier morceau est une erreur<a id="back" href="#asterisk">*</a> et tout ce qu'on peut y trouver d'autre c'est un vieil album vaseux. Ma page est dans mes <a href="#links">liens</a>.`,
+  links: "Liens",
+  "links-p": `Tous mes liens sont sur <a href="https://p6nj.carrd.co/" title="Carrd.co">Carrd</a> (page en anglais).`,
+  pics: "Photos",
+  "pics-p": "Voici quelques preuves que j'existe :",
+  "contact-telepathy": "Télépathie",
+  drawings: "Dessins (secret)",
+  "drawings-scared": "Ah ! Tu m'as fait peur.",
+  "drawings-p": `Welp, on dirait que t'as trouvé mes dessins. Ils viennent de carnets que j'avais entre mes 11 et 15 ans.<br />
+  J'en ai rempli 5, de 50 à 100 pages chacun.`,
+  "drawings-context":
+    "On pouvait me croiser à l'époque sur un banc de la cour de récréation, plié tel une crevette, concentré, absorbé, remplissant ces carnets de gribouillages au stylo, de jeux inspirés de magazines auquel je jouais tout seul et de petits essais sur la vie.",
+  "drawings-life":
+    "Une vie solitaire c'est dur mais à l'époque ça n'avait pas une grande importance.",
+  "drawings-caption":
+    "La goutte d'eau était mon personnage principal, ou plutôt une espèce : le Miramiro. Il est inspiré d'un dessin de l'aîné de mes frangins, qui est aussi mon peintre préféré.",
+  asterisk: `(*) Je fabriquais un interpréteur d'une espèce de MML (Music Macro Language) et j'essayais de construire un vibrato via l'expression mathématique d'un signal. Naturellement, j'ai glissé un sinus autour de la variable associée à la fréquence du signal (mais lui-même une sinusoïde !). Le résultat était un vibrato mais avec un plafond de plus en plus haut (sans limite...). Revenez là-haut : <a href="#back">↑</a>`,
+  feet: "Ce site est écrit avec 🦶 et Pure CSS v3.0.0",
+  secret: "Révéler tous les secrets",
+};
+
+document.getElementById("fr").onclick = (event) => {
+  event.target.src = "https://fish.golf/explosion.gif";
+  for (var key in translation) {
+    document.getElementById(key).innerHTML = translation[key];
+  }
+};
